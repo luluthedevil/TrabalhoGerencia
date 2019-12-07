477c94b5e05679cbe9e667bb8eb626c287265c27
Lulu <lucianaferreira91169@gmail.com>
2019-12-07 17:54:06 -0300

    Organizando o codigo com for e um array para as imagens

--- carros.js
+++ carros.js
@@ -1,29 +1,27 @@
-let xCarros = [550, 550, 550];
+let xCarros = [550, 550, 550, 550];
 let yCarros = [40, 96, 150];
 let velocidadeCarros = [2.9, 2.5, 3.2]
 let widthCarro = 50;
 let heightCarro = 40;
 
+var i;
+
 function mostraCarro(){
-  image(imgCarro1, xCarros[0], yCarros[0], widthCarro, heightCarro);
-  image(imgCarro2, xCarros[1], yCarros[1], widthCarro, heightCarro);
-  image(imgCarro3, xCarros[2], yCarros[2], widthCarro, heightCarro);
+  for(i=0; i<imgCarros.length; i++){
+    image(imgCarros[i], xCarros[i], yCarros[i], widthCarro, heightCarro);
+  }
 }
 
 function moveCarro(){
-  xCarros[0] = xCarros[0] - velocidadeCarros[0];
-  xCarros[1] = xCarros[1] - velocidadeCarros[1];
-  xCarros[2] = xCarros[2] - velocidadeCarros[2];
+  for(i=0; i<imgCarros.length; i++){
+    xCarros[i] = xCarros[i] - velocidadeCarros[i];
+  }
 }
 
 function voltaPosicaoInicialCarro(){
-  if(xCarros[0] < -40){
-    xCarros[0] = 550;
-  }
-  if(xCarros[1] < -40){
-    xCarros[1] = 550;
-  }
-  if(xCarros[2] < -40){
-    xCarros[2] = 550;
+  for(i=0; i<imgCarros.length; i++){
+    if(xCarros[i] < -40){
+      xCarros[i] = xCarros[xCarros.length - 1];
+    }
   }
 }
--- imagens.js
+++ imagens.js
@@ -4,6 +4,7 @@ let personagem;
 let imgCarro1;
 let imgCarro2;
 let imgCarro3;
+let imgCarros;
 
 
 function preload(){
@@ -13,4 +14,5 @@ function preload(){
     imgCarro1 = loadImage("img/carro-1.png");
     imgCarro2 = loadImage("img/carro-2.png");
     imgCarro3 = loadImage("img/carro-3.png");
+    imgCarros = [imgCarro1, imgCarro2, imgCarro3];
 }
