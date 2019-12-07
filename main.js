8885e2d877ef44eec52cffe63ce0cd3be90ce851
Mickael <araujomickael18@gmail.com>
2019-12-07 20:43:21 -0200

    carregando imagem do personagem na tela

--- main.js
+++ main.js
@@ -4,4 +4,5 @@ function setup() {
 
 function draw() {
   background(imagemFundo);
+  mostrarPersonagem();
 }
---
+++ personagem.js
@@ -0,0 +1,8 @@
+let xPersonagem = 200; //posição horizontal do personagem
+let yPersonagem = 370; //posição vertical do personagem
+
+let pontos = 0;
+
+function mostrarPersonagem() {
+    image(personagem, xPersonagem, yPersonagem, 30, 30);
+}
